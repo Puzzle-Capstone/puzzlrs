8d4f8797db0cd063c87a75f19815111c5d725f8a
jose Tovar <jose.tovar102397@gmail.com>
2022-02-03 16:09:37 -0700

    Add functional components

---
+++ src/components/AddPuzzleForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddPuzzleForm = () => {
+
+  return (
+    <section></section>
+  )
+}
+
+export default AddPuzzleForm;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nav = () => {
+
+  return (
+    <section></section>
+  )
+}
+
+export default Nav;
