9cd7aac755f77a54dd00e4b21436f1eef6ec30fa
Kyra Bergsund <k.bergsund@gmail.com>
2022-02-04 17:21:52 -0800

    Build user profile skeleton

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import '../css/UserProfile.css';
 
 const UserProfile = () => {
 
   return (
-    <section>user profile</section>
+    <section className='user-profile'>
+      <h2>Hi, _______!</h2>
+      <div className='profile-column-container'>
+        <section className='profile-column'>
+          <p>Your Puzzles</p>
+        </section>
+        <section className='profile-column'>
+          <p>Sent Requests</p>
+        </section>
+        <section className='profile-column'>
+          <p>Received Requests</p>
+        </section>
+      </div>
+    </section>
   )
 }
 
