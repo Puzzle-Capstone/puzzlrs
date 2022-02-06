d145e2ad4249736902979e06e0b82d52cf114719
jose Tovar <jose.tovar102397@gmail.com>
2022-02-06 16:09:16 -0700

    Add state for dropdowns

--- src/components/AddPuzzleForm.tsx
+++ src/components/AddPuzzleForm.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { Select, InputLabel, FormControl, MenuItem } from "@mui/material";
 import { categories, missingPieces } from "../utils";
 import '../css/AddPuzzleForm.css'
 
 const AddPuzzleForm = () => {
+
+  const [category, setCategory] = useState('');
+  const [pieceCount, setPieceCount] = useState('');
+
+  const categoryOptions = categories.map(category => {
+    return (
+      <MenuItem value={category} key={category}>{category}</MenuItem>
+    )
+  })
+
+  const piecesOptions = missingPieces.map(missingPiece => {
+    return (
+      <MenuItem value={missingPiece} key={missingPiece}>{missingPiece}</MenuItem>
+    )
+  })
 
   return (
     <section className='form-container'>
