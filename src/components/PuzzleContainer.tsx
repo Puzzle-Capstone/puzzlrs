cdeedfb14f1f2a1f7b68b7cbda395d15168a0488
Carly Collums <carlycollums@gmail.com>
2022-02-06 14:15:25 -0700

    Add puzzle page structure

--- src/components/PuzzleContainer.tsx
+++ src/components/PuzzleContainer.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React from 'react';
+import PuzzleDetails from "./PuzzleDetails";
+import '../css/PuzzleContainer.css';
+import Puzzle from './Puzzle';
 
 const PuzzleContainer = () => {
 
+  const dumbieData = [{ id: 1, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  {id: 2, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  {id: 3, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  {id: 4, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  {id: 5, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  {id: 6, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' }]
+
+  const puzzles = dumbieData.map(puzzle => 
+    <PuzzleDetails 
+      catagory={puzzle.catagory}
+      missingPieces={puzzle.missingPieces}
+      price={puzzle.price}
+      quality={puzzle.quality}
+      size={puzzle.size}
+      image={puzzle.image}
+      key={puzzle.id}
+      id={puzzle.id}
+    />)
+
   return (
-    <section>puzzle container pizza</section>
+    <section className='puzzles-container'>
+      {puzzles}
+    </section>
   )
 }
 
--- src/components/PuzzleDetails.tsx
+++ src/components/PuzzleDetails.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import '../css/PuzzleDetails.css';
 
-const PuzzleDetails = () => {
+interface puzzleDetails {
+  id: number
+  catagory: string
+  missingPieces: number
+  price: number
+  quality: string
+  size: number
+  image: string
+}
+
+const PuzzleDetails: React.FC<puzzleDetails> = ({ id, catagory, missingPieces, price, quality, size, image }) => {
 
   return (
-    <section></section>
+    <section className='individual-puzzle'>
+      <p>{id}</p>
+      <p>{catagory}</p>
+      <p>{missingPieces}</p>
+      <p>{price}</p>
+      <p>{quality}</p>
+      <p>{size}</p>
+      <p>{image}</p>
+    </section>
   )
 }
 
