8eace70982fd77c0c82e3a50a435131eaa6caf55
jose Tovar <jose.tovar102397@gmail.com>
2022-02-06 18:49:42 -0700

    Add options for select components

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,23 @@
-export const categories = ['Animals', 'Art', 'Mystery', 'Mythical', 'Nature', 'Funny', 'Travel'];
-export const missingPieces = ['0-10', '10-20', '20-30', '30-40', '40-50'];
+import { MenuItem } from "@mui/material";
+
+const categories = ['Animals', 'Art', 'Mystery', 'Mythical', 'Nature', 'Funny', 'Travel'];
+const missingPieces = ['0-10', '10-20', '20-30', '30-40', '40-50'];
+const conditions = ['Fair', 'Good', 'Very Good', 'Excellent']
+
+export const categoryOptions = categories.map(category => {
+  return (
+    <MenuItem value={category} key={category}>{category}</MenuItem>
+  )
+})
+
+export const piecesOptions = missingPieces.map(missingPiece => {
+  return (
+    <MenuItem value={missingPiece} key={missingPiece}>{missingPiece}</MenuItem>
+  )
+})
+
+export const qualityOptions = conditions.map(quality => {
+  return (
+    <MenuItem value={quality} key={quality}>{quality}</MenuItem>
+  )
+})
