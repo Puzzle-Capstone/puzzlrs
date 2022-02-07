946f0e88f7af27fcaa5bd1566f9354691700fee3
Kyra Bergsund <k.bergsund@gmail.com>
2022-02-07 10:51:07 -0800

    Refactor puzzle grid dropdowns with MUI

--- src/components/PuzzleContainer.tsx
+++ src/components/PuzzleContainer.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Select, InputLabel, FormControl, TextField, Input, InputAdornment } from "@mui/material";
+import { categoryOptions, piecesOptions, qualityOptions, pieceCountOptions } from "../utils";
 import '../css/PuzzleContainer.css';
 import Puzzle from './Puzzle';
 
 
 const PuzzleContainer = () => {
+  const [category, setCategory] = useState('');
+  const [pieceCount, setPieceCount] = useState('');
+  const [quality, setQuality] = useState('');
 
   const dumbieData = [{ id: 1, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
-  {id: 2, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
-  {id: 3, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1000, image: 'url' },
-  {id: 4, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1200, image: 'url' },
-  {id: 5, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 800, image: 'url' },
-  {id: 6, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1500, image: 'url' }]
+  { id: 2, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 500, image: 'url' },
+  { id: 3, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1000, image: 'url' },
+  { id: 4, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1200, image: 'url' },
+  { id: 5, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 800, image: 'url' },
+  { id: 6, catagory: 'easy', missingPieces: 1, price: 15, quality: 'good', size: 1500, image: 'url' }]
 
-  const puzzles = dumbieData.map(puzzle => 
-    <Puzzle 
+  const puzzles = dumbieData.map(puzzle =>
+
+    <Puzzle
       catagory={puzzle.catagory}
       missingPieces={puzzle.missingPieces}
       price={puzzle.price}
@@ -29,23 +35,36 @@ const PuzzleContainer = () => {
       <div className='filters'>
         <h3>Filter Puzzles</h3>
         <div>
-          <select>
-            <option value="" hidden>Catagory</option>
-            <option value='Animals'>Animals</option>
-            <option value='Colors'>Colors</option>
-          </select>
-          <select>
-            <option value="" hidden>Piece Count</option>
-            <option value='500'>500</option>
-            <option value='1000'>1000</option>
-            <option value='1500'>1500</option>
-          </select>
-          <select>
-            <option value="" hidden>Difficulty</option>
-            <option value='Easy'>Easy</option>
-            <option value='Intermediate'>Intermediate</option>
-            <option value='Hard'>Hard</option>
-          </select>
+          <FormControl variant="standard">
+            <InputLabel>Category</InputLabel>
+            <Select
+              className='puzzle-grid-dropdown'
+              value={category}
+              onChange={event => setCategory(event.target.value)}
+            >
+              {categoryOptions}
+            </Select>
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel>Piece Count</InputLabel>
+            <Select
+              className='puzzle-grid-dropdown'
+              value={pieceCount}
+              onChange={event => setPieceCount(event.target.value)}
+            >
+              {pieceCountOptions}
+            </Select>
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel>Quality</InputLabel>
+            <Select
+              className='puzzle-grid-dropdown'
+              value={quality}
+              onChange={event => setQuality(event.target.value)}
+            >
+              {qualityOptions}
+            </Select>
+          </FormControl>
         </div>
       </div>
       <section className='puzzles-container'>
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,7 @@ import { MenuItem } from "@mui/material";
 const categories = ['Animals', 'Art', 'Mystery', 'Mythical', 'Nature', 'Funny', 'Travel'];
 const missingPieces = ['0-10', '10-20', '20-30', '30-40', '40-50'];
 const conditions = ['Fair', 'Good', 'Very Good', 'Excellent']
+const pieceCount = ['0-99', '100-499', '500-999', '1000-1499', '1500+']
 
 export const categoryOptions = categories.map(category => {
   return (
@@ -19,5 +20,11 @@ export const piecesOptions = missingPieces.map(missingPiece => {
 export const qualityOptions = conditions.map(quality => {
   return (
     <MenuItem value={quality} key={quality}>{quality}</MenuItem>
+  )
+})
+
+export const pieceCountOptions = pieceCount.map(pieceCount => {
+  return (
+    <MenuItem value={pieceCount} key={pieceCount}>{pieceCount}</MenuItem>
   )
 })
