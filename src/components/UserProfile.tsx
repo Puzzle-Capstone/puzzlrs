e49d9c44c7dffe7864939f692357092a5ebbfaea
Kyra Bergsund <k.bergsund@gmail.com>
2022-02-06 20:51:50 -0800

    Add placeholder user profile puzzle components

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import UserProfilePuzzle from './UserProfilePuzzle';
 import '../css/UserProfile.css';
 
 const UserProfile = () => {
 
   return (
     <section className='user-profile'>
-      <h2>Hi, _______!</h2>
+      <h2>Hi, Kyra!</h2>
       <div className='profile-column-container'>
         <section className='profile-column'>
           <p>Your Puzzles</p>
+          <div className='user-puzzles'>
+            <UserProfilePuzzle />
+          </div>
         </section>
-        <section className='profile-column'>
+        <section className='profile-column center'>
           <p>Sent Requests</p>
+          <div className='user-puzzles'>
+            <UserProfilePuzzle />
+            <UserProfilePuzzle />
+            <UserProfilePuzzle />
+            <UserProfilePuzzle />
+          </div>
         </section>
         <section className='profile-column'>
           <p>Received Requests</p>
+          <div className='user-puzzles'>
+            <UserProfilePuzzle />
+            <UserProfilePuzzle />
+          </div>
         </section>
       </div>
     </section>
