38aba40aa96f1d8edd4809cfde4b7016dd56d266
Carly Collums <carlycollums@gmail.com>
2022-02-10 19:48:14 -0700

    Add puzzle icon to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import '../css/Home.css';
+import { GiPuzzle } from "react-icons/gi";
 
 const Home = () => {
   return (
     <section className="homepage">
+      <GiPuzzle size={450} className='puzzle-icon'/>
       <h2>Tired of your puzzles? Trade with people like you!</h2>
       <Link to='/puzzles'><button>View Puzzles</button></Link>
     </section>
